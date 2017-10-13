3657dc1607a1a89ec9b89bc79817bb938f2c6a9d
egor <egor.krutoychel.komp@mail.ru>
2017-10-13 20:16:01 +0300

    refactored code, without threads

--- Server/main.c
+++ Server/main.c
@@ -9,39 +9,36 @@
 #include <zconf.h>
 
 #define PORT 5223
-#define BUFSIZE 500
+#define BUFSIZE 2000
 #define TRUE 1
-#define FALSE !TRUE
 
-int boolean = TRUE;
-char *serverBuf;
+char serverBuf[BUFSIZE];
 
 struct threadDataStruct {
     int clientSocket;
     char *clientIp;
 };
 
-void *threadFunc(void *voidPtr) {
-    struct threadDataStruct* threadData = (struct threadDataStruct*) voidPtr;
-
-    int socket = threadData->clientSocket;
-    char *ip = threadData->clientIp;
-    char *string = (char*) malloc(sizeof(char) * BUFSIZE);
-    sprintf(string, "[%d]: accept new client %s\n", socket, ip);
-
-    for (int i = 5; i < 5; i++) {
-        char *newString = (char*) malloc(sizeof(char) * BUFSIZE);
-        sprintf(newString, "[%d]: idle\n", socket);
-        strcat(string, newString);
-        free(newString);
-    }
-
-    strcat(serverBuf, string);
-
-    free(string);
-    free(threadData);
-    free(ip);
-}
+//void *threadFunc(void *voidPtr) {
+//    struct threadDataStruct* threadData = (struct threadDataStruct*) voidPtr;
+//    int socket = threadData->clientSocket;
+//    char *ip = threadData->clientIp;
+//    char *string = (char*) malloc(sizeof(BUFSIZE));
+//    sprintf(string, "[%d]: accept new client %s\n", socket, ip);
+//
+//    for (int i = 0; i < 5; i++) {
+//        char *newString = (char*) malloc(sizeof(BUFSIZE));
+//        sprintf(newString, "[%d]: idle\n", socket);
+//        strcat(string, newString);
+//        free(newString);
+//    }
+//
+//    strcat(serverBuf, string);
+//
+//    free(string);
+//    free(threadData);
+//    free(ip);
+//}
 
 int initServerSocket(char *ip) {
     struct sockaddr_in addr;
@@ -53,10 +50,9 @@ int initServerSocket(char *ip) {
     addr.sin_addr.s_addr = inet_addr(ip);
     bind(serverSocket, (struct sockaddr *)&addr, sizeof(addr));
     listen(serverSocket, 5);
-
+    free(ip);
     return serverSocket;
 }
-
 int initClientSocket(struct sockaddr_in clientAddr, int serverSocket) {
     int clientLen = sizeof(clientAddr);
     return accept(serverSocket, (struct sockaddr *) &clientAddr, &clientLen);
@@ -71,44 +67,48 @@ struct threadDataStruct *initStruct(struct sockaddr_in clientAddr, int clientSoc
 
 int main(void) {
     struct sockaddr_in clientAddr;
-    serverBuf = (char*) malloc(sizeof(char) * BUFSIZE);
 
-    char *ip = (char*) malloc(sizeof(char) * BUFSIZE);
-    fgets(ip, BUFSIZE, stdin);
+    char *ip = (char*) malloc(sizeof(char) * 20);
+    fgets(ip, 20, stdin);
     int serverSocket = initServerSocket(ip);
-    free(ip);
 
     while (TRUE) {
         int clientSocket = initClientSocket(clientAddr, serverSocket);
-        struct threadDataStruct *threadData = initStruct(clientAddr, clientSocket);
 
+        struct threadDataStruct *threadData = initStruct(clientAddr, clientSocket);
 //        pthread_t pthread;
 //        pthread_create(&pthread, NULL, threadFunc, (void*)threadData);
 //        pthread_join(pthread, NULL);
-//        boolean = FALSE;
 
-        char buf[sizeof(BUFSIZE)];
+        char buf[BUFSIZE];
         int byteRead = recv(clientSocket, buf, BUFSIZE - 1, 0);
         buf[byteRead] = '\0';
 
-        if (strcmp(buf, "INT") == 0) {
-            FILE *file = fopen("/tmp/buf.txt","r");
+        if (strcmp(buf, "INT\n") == 0) {
+            FILE *file = fopen("tmp/buf.txt","w");
             fwrite(serverBuf, sizeof(serverBuf), 1, file);
-            printf("buf.txt");
-            free(serverBuf);
-            free(threadData);
+            fclose(file);
+
+            //free(threadData);
+
+            memset(serverBuf, 0, sizeof(serverBuf));
             close(clientSocket);
-            serverBuf = (char*) malloc(sizeof(char) * BUFSIZE);
+
+            char *filename = (char *) malloc(sizeof(char) * 100);
+            sprintf(filename, "tmp/buf.txt\n");
+            printf("%s", filename);
+            free(filename);
         } else {
             strcat(serverBuf, buf);
             send(clientSocket, buf, strlen(buf), 0);
 
-            char *string = (char *) malloc(sizeof(char) * BUFSIZE);
+            //free(threadData);
+
+            char *string = (char *) malloc(sizeof(char) * 100);
             sprintf(string, "[%d]: client %s disconnected\n", clientSocket, inet_ntoa(clientAddr.sin_addr));
             strcat(serverBuf, string);
-
             free(string);
-            free(threadData);
+
             close(clientSocket);
         }
     }
